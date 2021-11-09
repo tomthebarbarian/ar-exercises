a8cc656821c514fc03c04366a9c23487f76aedf6
Tom Zhang <tom.zhang989@gmail.com>
2021-11-09 12:40:05 -0500

    completes excersise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+class Stores < ActiveRecord::Base
+
+  @store1 = Stores.find_by(id: 1)
+  @store2 = Stores.find_by(id: 2)
+
+  @store1.name = "Burnaby"
+  @store1.save
+end
