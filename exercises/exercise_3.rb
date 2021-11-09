df4b3efca5e3425afec7815aa3cd636803eb68e4
Tom Zhang <tom.zhang989@gmail.com>
2021-11-09 12:41:20 -0500

    deletes a store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,12 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+class Stores < ActiveRecord::Base
+
+  @store3 = Stores.find_by(id: 3)
+  @store3.destroy
+
+  stores_num = Stores.all.count
+
+  puts "This is stores num #{stores_num}"
+end
